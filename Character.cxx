9ad4957addf6a75d29aaa0afba0c45f15e2e4421
TLeBourhis <48522811+TLeBourhis@users.noreply.github.com>
2019-12-04 17:04:24 +0100

    Changement du Character.cxx qui était en fait Champion.h

--- Character.cxx
+++ Character.cxx
@@ -1,40 +1,59 @@
 #include "Character.h"
-#include "Race.h"
 #include "Param.h"
-#include <list>
+#include <cmath>
+#include <string>
 
-#ifndef CHAMPION_H
-#define CHAMPION_H
+using namespace std;
 
-class Champion : public Character{
-private:
-  int level;
-  //Race *races[2];
-  int cost;
+int Character::staticID = 0;
 
-public:
-  virtual void display() const;
-  void fight(std::list<Character*> allies, std::list<Character*> ennemies);
-  void displayCard(int i) const;
-  void evolve();
-  void dragndrop(int _x, int _y);
-  int buy();
-  int sell();
-  void move(int _x, int _y);
-  void move(Character *target);
-  int die();
-  void setColor(){color[0] = Param::championColor[0]; color[1] = Param::championColor[1]; color[2] = Param::championColor[2]; color[3] = Param::championColor[3];};
-  //void spell(Monster *monster);
+Character::Character(std::string _name, int _armor, int _magicResistance, float _spellPower, int _attackRange, float _attackSpeed, int _health, int _attackDamage, int _i, int _j, int _caseID){
+  staticID++;
+  id = staticID;
+
+  name = _name;
+  armor = _armor;
+  magicResistance = _magicResistance;
+  spellPower = _spellPower;
+  attackRange = _attackRange;
+  attackSpeed = _attackSpeed;
+  alive = true;
+  health = _health;
+  attackDamage = _attackDamage;
+  i = _i;
+  j = _j;
+  caseID = _caseID;
+  color[0] = 0.0f;
+  color[1] = 0.0f;
+  color[2] = 0.0f;
+  color[3] = 1.0f;
+  //color = _color;
+}
 
 
+void Character::getHit(int damage, string type){
+  if(type == "ATTACK_DAMAGE"){
+    health -= (damage - armor);
+    if(health <= 0){
+      this->die();
+    }
+  }else{
+    health -= (damage - magicResistance);
+    if(health <= 0){
+      this->die();
+    }
+  }
+}
 
-  //Constructeurs
-  Champion(std::string _name, int _armor, int _magicResistance, float _spellPower, int _attackRange, float _attackSpeed, int _health, int _attackDamage, int _i, int _j, int _caseID, int _level, /*Race _races[2],*/ int cost);
+float Character::distance(Character * c) const{
+  int _xc = c->i * (Param::dimCase + 2*Param::borderSpacingCase) + Param::borderSpacingCase + Param::dimCase/2;
+  int _yc = c->j * (Param::dimCase + 2*Param::borderSpacingCase) + Param::borderSpacingCase + Param::dimCase/2;
+  int _xme = this->i * (Param::dimCase + 2*Param::borderSpacingCase) + Param::borderSpacingCase + Param::dimCase/2;
+  int _yme = this->j * (Param::dimCase + 2*Param::borderSpacingCase) + Param::borderSpacingCase + Param::dimCase/2;
+  return sqrt(pow(_xc-_xme,2) + pow(_yc-_yme,2));
+}
 
-  //Destructeur
-  ~Champion();
-
-  //Accesseurs
-};
-
-#endif /* end of include guard: CHAMPION_H */
+void Character::moveTo(int _x, int _y){
+  this->i = (int)(_x / (Param::dimCase + 2*Param::borderSpacingCase));
+  this->j = (int)(_y/ (Param::dimCase + 2*Param::borderSpacingCase));
+}

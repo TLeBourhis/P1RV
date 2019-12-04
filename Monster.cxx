88fe648a7bd304e8ea10fda64da4c0bc4a04ae9a
TLeBourhis <48522811+TLeBourhis@users.noreply.github.com>
2019-12-04 15:46:36 +0100

    Update Monster.cxx

--- Monster.cxx
+++ Monster.cxx
@@ -1,18 +1,22 @@
-#include "headers/Monster.h"
+#include "Monster.h"
+#include "Param.h"
 #include <string>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
 
 using namespace std;
 
-Monster::Monster(string _name, int _armor, int _magicResistance, float _spellPower, int _attackRange, float _attackSpeed, int _health, int _attackDamage, int _x, int _y, int _caseID, int _gold, int _rage) : Character(_name,_armor,_magicResistance, _spellPower, _attackRange, _attackSpeed, _health, _attackDamage, _x, _y, _caseID){
+Monster::Monster(string _name, int _armor, int _magicResistance, float _spellPower, int _attackRange, float _attackSpeed, int _health, int _attackDamage, int _i, int _j, int _caseID, int _gold, int _rage) : Character(_name,_armor,_magicResistance, _spellPower, _attackRange, _attackSpeed, _health, _attackDamage, _i, _j, _caseID){
   gold = _gold;
   rage = _rage;
+  setColor();
 }
 
 void Monster::fight(list<Character*> allies, list<Character*> ennemies){
   //Gère les actions du champion lors du combat.
 
 
-  if(size(ennemies) == 0){
+  if(ennemies.size() == 0){
     //Si pas d'ennemi
 
 
@@ -48,7 +52,7 @@ void Monster::fight(list<Character*> allies, list<Character*> ennemies){
   }
 }
 
-void Monster::boostedAttack(Champion *champion) const{
+void Monster::boostedAttack(Character *champion) const{
   //Inflige le double des dégâts d'auto-attaque au champion adverse
 
   champion->getHit(2*this->attackDamage,"ATTACK_DAMAGE");
@@ -59,3 +63,22 @@ int Monster::die(){
   this->alive = false;
   return gold;
 }
+
+void Monster::move(Character *target){
+
+};
+
+void Monster::display() const{
+  glMatrixMode(GL_MODELVIEW);
+
+  glPushMatrix();
+
+  glLoadIdentity();
+
+  glColor4f(color[0],color[1],color[2],color[3]);
+
+  glTranslatef(i*(2*Param::borderSpacingCase+Param::dimCase)+Param::borderSpacingCase+Param::dimCase/2, 4.0f, j*(2*Param::borderSpacingCase+Param::dimCase)+Param::borderSpacingCase+Param::dimCase/2);
+  glutSolidSphere(4.0f,20,20);
+
+  glPopMatrix();
+};
